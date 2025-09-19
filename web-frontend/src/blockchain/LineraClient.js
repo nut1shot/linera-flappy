cec291a5454d5c5313fc4d72ea4d34b6a5a1a5c2
nut1shot <nutp10.1@gmail.com>
2025-09-19 12:02:45 +0700

    create new wallet when start

--- web-frontend/src/blockchain/LineraClient.js
+++ web-frontend/src/blockchain/LineraClient.js
@@ -90,8 +90,7 @@ export class LineraClient {
       console.log("faucet", this.APP_URL);
       // this.mnemonic =
       //   this.getMnemonic() || ethers.Wallet.createRandom().mnemonic.phrase;
-      this.mnemonic =
-        this.getMnemonic() || ethers.Wallet.createRandom().mnemonic.phrase;
+      this.mnemonic = ethers.Wallet.createRandom().mnemonic.phrase;
       const signer = PrivateKey.fromMnemonic(this.mnemonic);
       console.log("signer", signer);
       this.wallet = await this.faucet.createWallet();
